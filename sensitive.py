e71931b221174a76314a0ea70d461e6dfa08ba6f
colin99d <72827203+colin99d@users.noreply.github.com>
2021-02-11 21:55:00 -0500

    Update sensitive.py

--- sensitive.py
+++ sensitive.py
@@ -1,4 +1,10 @@
 class LoginInfo:
-    def __init__(self, name):   
+    def __init__(self):   
         self.username = "username"
         self.password = "password"
+
+    def get_username(self):
+        return self.username
+    
+    def get_password(self):
+        return self.password
