2f3fef6df622b5d9adf6c2259dc613520e2eeae1
Colin Delahunty <colindelahunty@Colins-MacBook-Air.local>
2021-04-12 21:25:14 -0400

    Improved option analysis

--- optionVariance.py
+++ optionVariance.py
@@ -10,9 +10,9 @@ class OptionManager():
         self.optionsChains = {}
         self.callChains = {}
         self.putChains = {}
-        self.callChainsFiltered = {}
-        self.putChainsFiltered = {}
-        self.bestFitLines = {}
+        self.callChainsF = {}
+        self.putChainsF = {}
+        self.variances = {}
 
     def addExpirations(self):
         self.expirations = self.stock.options
@@ -21,9 +21,23 @@ class OptionManager():
         chains = self.stock.option_chain(expiration)
         self.optionsChains[expiration] = chains
         self.callChains[expiration] = chains.calls
-        self.callChainsFiltered[expiration] = chains.calls[chains.calls['percentChange'] != 0]
         self.putChains[expiration] = chains.puts
-        self.putChainsFiltered[expiration] = chains.puts[chains.puts['percentChange'] != 0]
+        self.cleanData(expiration)
+
+    def cleanData(self, expiration):
+        #Remove $0 Ask price
+        self.callChainsF[expiration] = self.callChains[expiration][self.callChains[expiration]['ask'] != 0]
+        self.putChainsF[expiration] =  self.putChains[expiration][ self.putChains[expiration]['ask'] != 0]
+        #Only show in the money
+        self.callChainsF[expiration] = self.callChainsF[expiration][self.callChainsF[expiration]['inTheMoney'] == True]
+        self.putChainsF[expiration] =  self.putChainsF[expiration][ self.putChainsF[expiration]['inTheMoney'] == True]
+        #Open interest greater than 0
+        self.callChainsF[expiration] = self.callChainsF[expiration][self.callChainsF[expiration]['openInterest'] > 0]
+        self.putChainsF[expiration] =  self.putChainsF[expiration][self.putChainsF[expiration]['openInterest'] > 0]
+        #Regular contract size
+        self.callChainsF[expiration] = self.callChainsF[expiration][self.callChainsF[expiration]['contractSize'] == "REGULAR"]
+        self.putChainsF[expiration] =  self.putChainsF[expiration][ self.putChainsF[expiration]['contractSize'] == "REGULAR"]
+
 
     def saveCSV(self,expiration):
         try:
@@ -31,31 +45,36 @@ class OptionManager():
         except PermissionError:
             print("Data not saved, close CSV before running")
 
-    def bestFitLine(self,expiration):
+    def graphLines(self,expiration):
         if self.contract == "call":
-            x = np.array(self.callChains[expiration].strike)
-            y = np.array(self.callChains[expiration].ask)
+            x = np.array(self.callChainsF[expiration].strike)
+            y = np.array(self.callChainsF[expiration].ask)
+            for i in range(len(y)-1):
+                if y[i] > y[i+1]:
+                    if self.variances[expiration]:
+                        self.variances[expiration] =[{"ask":y[i], "strike":x[i], "dif":(y[i+1]-y[i])/y[i]}]
+                    else:
+                        self.variances[expiration].append({"ask":y[i], "strike":x[i], "dif":(y[i+1]-y[i])/y[i]})
+                    
         elif self.contract == "put":
-            x = np.array(self.putChains[expiration].strike)
-            y = np.array(self.putChains[expiration].ask)
+            x = np.array(self.putChainsF[expiration].strike)
+            y = np.array(self.putChainsF[expiration].ask)
+            for i in range(len(y)-1):
+                if y[i] < y[i+1]:
+                    if self.variances[expiration]:
+                        self.variances[expiration] =[{"ask":y[i], "strike":x[i], "dif":(y[i+1]-y[i])/y[i]}]
+                    else:
+                        self.variances[expiration].append({"ask":y[i], "strike":x[i], "dif":(y[i+1]-y[i])/y[i]})
 
-        plt.plot(x, y, marker='o',label="Act-"+str(expiration))
-        """
-        fit = np.polyfit(x, y, 2)
-        print(fit)
-        plt.plot(x, fit[0]*(x**2) + fit[1]*x + fit[2])
-        """
-        fit = np.polyfit(np.log(x), y, 1)
-        print(fit)
-        #plt.plot(x, fit[0]*np.log(x) + fit[1])
-        
-        self.bestFitLines[expiration] = fit
+        plt.plot(x, y, marker='o',label=str(expiration))
 
     def createData(self):
         self.addExpirations()
-        for expiration in self.expirations:
+        for expiration in self.expirations[6:]:
             self.addOptionsChain(expiration)
-            self.bestFitLine(expiration)
+            self.graphLines(expiration)
+        plt.xlabel("Option Strike Price ($)")
+        plt.ylabel("Option Ask Price ($)")
         plt.title(self.ticker + " " + self.contract.capitalize() + "s")
         plt.legend()
         plt.show()
